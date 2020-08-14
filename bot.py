12157c7178dc39af365670488fb9503fc5abbc7c
Alexander Baskikh <40061352+baskiton@users.noreply.github.com>
2020-08-14 16:13:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,18 +3,17 @@ from os import environ
 from flask import Flask, request
 from telebot import types, TeleBot
 from random import randint, choice
-from requests import get
+from requests import get, RequestException
 
 
 TOKEN = environ.get('API_TOKEN')
-YA_API = environ.get('YA_API')
-my_domain = '<your_domain>'
+my_domain = 'baskitontgbot.herokuapp.com'
 
 bot = TeleBot(TOKEN)
 server = Flask(__name__)
 
 keyboard1 = types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('🎁 Рандомайзер', '🎲 Игральная кость', '🎭 Орёл-решка')
+keyboard1.row('🎁 Рандомайзер', '🎲 Кость', '🎭 Орёл-решка', '❓ Число')
 
 
 def rand_func(a, b):
@@ -22,33 +21,17 @@ def rand_func(a, b):
 
 
 def numbers_api(num):
+    api_url = 'http://numbersapi.com/{}/trivia'
+    params = {
+        'json': True,
+        'notfound': 'floor'
+    }
     try:
-        api_url = 'http://numbersapi.com/{}/trivia'
-        params = {
-            'json': True,
-            'notfound': 'floor'
-        }
         res = get(api_url.format(num), params=params)
         res.raise_for_status()
         if res.status_code == 200:
-            return ya_translate(res.json()['text'])
-    except BaseException as e:
-        return e
-
-
-def ya_translate(text):
-    try:
-        url = r'https://translate.yandex.net/api/v1.5/tr.json/translate'
-        params = {
-            'key': YA_API,
-            'text': text,
-            'lang': 'en-ru'
-        }
-        res = get(url, params=params)
-        res.raise_for_status()
-        if res.status_code == 200:
-            return res.json()['text'][0]
-    except BaseException as e:
+            return res.json()['text']
+    except RequestException as e:
         return e
 
 
@@ -65,11 +48,17 @@ def start_message(message):
 
 @bot.message_handler(regexp=r"^-?(\d+)\s-?(\d+)$", content_types=['text'])
 def random_bone(message):
-    a, b = (int(float(i)) for i in message.text.lower().split())
+    a, b = (int(i) for i in message.text.lower().split())
     if a > b:
         a, b = b, a
     num = rand_func(a, b)
     bot.send_message(message.chat.id, num)
+    bot.send_message(message.chat.id, numbers_api(num), reply_markup=keyboard1)
+
+
+@bot.message_handler(regexp=r"^-?(\d+)$", content_types=['text'])
+def number_info(message):
+    num = int(message.text.lower())
     bot.send_message(message.chat.id, numbers_api(num), reply_markup=keyboard1)
 
 
@@ -79,12 +68,16 @@ def send_text(message):
         bot.send_message(message.chat.id,
                          'Введите два числа из необходимого '
                          'диапазона через пробел. Например: 16 48')
-    elif message.text.lower() == '🎲 игральная кость':
+    elif message.text.lower() == '🎲 кость':
         bot.send_message(message.chat.id, rand_func(1, 6),
                          reply_markup=keyboard1)
     elif message.text.lower() == '🎭 орёл-решка':
         mon = choice(['Орёл', 'Решка'])
         bot.send_message(message.chat.id, mon, reply_markup=keyboard1)
+    elif (message.text.lower() == '❓ число' or
+          message.text.lower() == 'random'):
+        bot.send_message(message.chat.id, numbers_api('random'),
+                         reply_markup=keyboard1)
 
 
 @server.route('/' + TOKEN, methods=['POST'])
@@ -103,4 +96,6 @@ def webhook():
 
 
 if __name__ == "__main__":
+    # bot.remove_webhook()
+    # bot.set_webhook(url='https://%s/%s' % (my_domain, TOKEN))
     server.run(host="0.0.0.0", port=int(environ.get('PORT', 5000)))
